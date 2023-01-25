e8445fdc01b90e0b80e5c9d584dcd8d7704e09d9
Oswald Chartrain <72608635+Oswalld@users.noreply.github.com>
2023-01-25 23:06:35 +0100

    testing on githum pages

--- JS/slider.js
+++ JS/slider.js
@@ -1,3 +1,24 @@
-const arrowLeft = document.querySelector('.left-arrow');
-const arrowRight = document.querySelector('.right-arrow');
-const slider = document.querySelector('.slider');
+    // Swiper config
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    grabCursor: true,
+    initialSlide: 2,
+    spaceBetween: 15,
+    breakpoints: {
+      768: {
+        spaceBetween: 30
+      }
+    }
+  });
+  
+  const prevSlideBtn = document.querySelector(".button--control.prev");
+  const nextSlideBtn = document.querySelector(".button--control.next");
+  
+  prevSlideBtn.addEventListener("click", () => {
+    swiper.slidePrev();
+  });
+  nextSlideBtn.addEventListener("click", () => {
+    swiper.slideNext();
+  });
