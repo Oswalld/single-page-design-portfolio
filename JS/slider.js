0c1f8f63d9ba2d5127f67510bc1f88c7f1c8eff2
Oswald Chartrain <72608635+Oswalld@users.noreply.github.com>
2023-01-26 19:24:34 +0100

    js changes

--- JS/slider.js
+++ JS/slider.js
@@ -1,24 +1,27 @@
+  window.onload = function(event) {
     // Swiper config
-  const swiper = new Swiper(".swiper", {
-    loop: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    grabCursor: true,
-    initialSlide: 2,
-    spaceBetween: 15,
-    breakpoints: {
-      768: {
-        spaceBetween: 30
-      }
-    }
-  });
-  
-  const prevSlideBtn = document.querySelector(".button--control-prev");
-  const nextSlideBtn = document.querySelector(".button--control-next");
-  
-  nextSlideBtn.addEventListener("click", () => {
-    swiper.slideNext();
-  });
-  prevSlideBtn.addEventListener("click", () => {
-    swiper.slidePrev();
-  });
+     const swiper = new Swiper(".swiper", {
+       loop: true,
+       centeredSlides: true,
+       slidesPerView: "auto",
+       grabCursor: true,
+       initialSlide: 2,
+       spaceBetween: 15,
+       breakpoints: {
+         768: {
+           spaceBetween: 30
+         }
+       }
+     });
+     
+     const prevSlideBtn = document.querySelector(".button--control-prev");
+     const nextSlideBtn = document.querySelector(".button--control-next");
+     
+     nextSlideBtn.addEventListener("click", () => {
+       swiper.slideNext();
+     });
+     prevSlideBtn.addEventListener("click", () => {
+       swiper.slidePrev();
+     });
+
+  };
